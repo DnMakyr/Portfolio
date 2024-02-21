92428f6bfce390fad66eea97c99a601e635e23af
DnMakyr <107171443+NguyenLDHE176088@users.noreply.github.com>
2024-02-21 11:29:41 +0700

    Init

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-gradient-to-r from-white from-60% to-slate-500 h-screen">
+    <div>
+      <div>
+        <h1 class="text-9xl ">DN</h1>
+        <h2 class="text-4xl">Dai Nguyen</h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
